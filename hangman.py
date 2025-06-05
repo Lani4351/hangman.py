39403b1ab7c85b1cdf48f5d11cf2a63e1541d485
Lani4351 <natenunez967@gmail.com>
2025-06-04 20:18:29 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -7,8 +7,8 @@ def print_welcome_message():
     print("Welcome to my Hangman game!")
 
 def generate_random_word():
-    words_list = ["elephant", "monster", "zebra", "saxophone", "clarinet",
-               "sashimi", "perpendicular", "bat"]
+    words_list = ["stars", "dreams", "love", "astronomy", "mango",
+               "minecraft", "jaguar", "calico"]
     return random.choice(words_list)
 
 def generate_blanks(word_input):
@@ -21,16 +21,26 @@ def display_current_blanks(blanks_input):
     blanks_display = ""
     for char in blanks_input:
         blanks_display += char + " "
+    print(f"Current blanks: {blanks_display}")
 
 def get_user_answer():
+    """This allows the user to guess a letter to play the game"""
     letter_guess = input("Please enter a letter guess: ")
     return letter_guess
 
-def check_validity():
-    pass
+def check_validity(secret_word, letter_guess):
+    return letter_guess in secret_word
 
-def update_blanks():
-    pass
+
+def update_blanks(secret_word, current_blanks, letter_guess):
+    """This function goes through the user guess and updates the blanks depending on whether it was correct or not."""
+    new_blanks = ""
+    for index in range(len(secret_word)):
+        if(secret_word[index] == letter_guess):
+            new_blanks += letter_guess
+        else:
+            new_blanks += current_blanks[index]
+    return new_blanks
 
 def display_game_won_message():
     print("You have won, congratualions!")
@@ -43,11 +53,12 @@ def run_game_loop():
    print_welcome_message()
    lives = 5
    random_word = generate_random_word()
+   print(f"My secret random word is '{random_word}'.")
    blanks = generate_blanks(random_word)
    while playing:
-       display_current_blanks()
+       display_current_blanks(blanks)
        answer = get_user_answer()
-       answer_valid = check_validity()
+       answer_valid = check_validity(random_word, answer)
        if(answer_valid):
            blanks = update_blanks(random_word, blanks, answer)
            if(blanks == random_word):
